fb513978c378ed047cf6ab82827a420834dd40f0
naivoder <credovia@students.kennesaw.edu>
2020-05-24 10:55:51 -0400

    work on binsearch wrapper

--- binarysearch/binarysearch.py
+++ binarysearch/binarysearch.py
@@ -1,8 +1,11 @@
 import binarysearch, random
+import numpy as np
+cimport numpy as np
 
 if __name__=="__main__":
   print("###---Binary Search with Cython---###")
   test_data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
   print(test_data)
   print(len(test_data))
-  binarysearch._search(test_data, 20, 19)
+  collection = np.array(test_data)
+  binarysearch._search(collection, 20, 19)
--- binarysearch/binarysearch.pyx
+++ binarysearch/binarysearch.pyx
@@ -1,9 +1,12 @@
-from cpython cimport array
-import array
+
+cimport cython
+from libc.stdlib import malloc
 
 cdef extern from "binarysearch.h":
     int binary_search(int collection[], int length, int target)
 
-def _search(list collection, length, target):
-    cdef array.array list = array.array('f', collection)
-    return binary_search(list.data.as_ints, length, target)
+def _search(collection, length, target):
+    array = <int *>malloc(len(collection)*cython.sizeof(collection))
+    for i in xrange(len(collection)):
+        array[i] = collection[i]
+    return binary_search(array, length, target)
